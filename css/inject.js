238a8867bc156022ee8c4ea39175ff928264fab2
David Alsh <alshdavid@gmail.com>
2020-01-15 14:20:21 +0700

    things

--- css/inject.js
+++ css/inject.js
@@ -1,28 +1,21 @@
 const styleElement = document.createElement('style')
 styleElement.innerHTML = /*css*/`
-  #readme {
-    position: absolute; 
-    top: 0; 
-    left: 0; 
-    right: 0; 
-    bottom: 0; 
-    z-index: 100; 
-    background-color: white;
-  }
-
   * {
     font-family: Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji;
   }
 
-  page-break {
+  ins {
+    display: block;
     page-break-after: always;
   }
-
-  @media print {
-    no-print {
-      display: none;
-    }
-  }
 `
+const insElements = Array.from(document.querySelectorAll('ins'))
+
+for (const insElement of insElements) {
+  const parent = insElement.parentNode
+  parent.parentNode.insertBefore(insElement, parent.nextSibling);
+  parent.parentNode.removeChild(parent)
+}
+
 document.head.appendChild(styleElement)
-document.body.appendChild(document.querySelector('#readme'))
+document.body.innerHTML = document.querySelector('#readme').innerHTML
