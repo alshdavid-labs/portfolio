68f2c2699499fcc31186bed2de2e4b1c34bc43fb
David Alsh <david.alsh@rokt.com>
2020-03-10 12:17:10 +1100

    ok

--- .github/print/main.js
+++ .github/print/main.js
@@ -3,7 +3,69 @@ let fs = require('fs');
 const FormData = require('form-data'); 
 const puppeteer = require('puppeteer');
 
-(async () => {
+const URL = 'https://api.github.com/repos/alshdavid/portfolio/releases'
+const token = process.env.GITHUB_TOKEN || 'not-set'
+
+void async function (){
+  const draftResponse = await createDraftRelease()
+  console.log(draftResponse)
+  // const { buffer, stats } = await createPDF()
+  // await uploadToRelease(draftResponse.upload_url, buffer, stats)
+  // await publishRelease(draftResponse.id)
+}();
+
+function createDraftRelease() {
+  return fetch(URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `token ${token}`
+    },
+    body: JSON.stringify({
+      tag_name: 'latest',
+      name: 'David Alsh: Resume',
+      body: 'Latest copy of my resume, automatically compiled from the markdown files in the repository',
+      target_commitish: 'master',
+      draft: true
+    })
+  }).then(r => r.json())
+}
+
+function uploadToRelease(upload_url, buffer, stats) {
+  const form = new FormData();
+
+  const uploadUrl = upload_url.replace('{?name,label}', '?name=resume.pdf')
+
+  form.append('file', buffer, {
+    contentType: 'application/pdf',
+    name: 'file',
+    knownLength: stats.size,
+    filename: 'resume.pdf',
+  });
+
+  return fetch(uploadUrl, {
+    method: 'POST',
+    headers: {
+      'Authorization': `token ${token}`,
+    },
+    body: form
+  }).then(r => r.json())
+}
+
+function publishRelease(id) {
+  return fetch(`${URL}/${id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `token ${token}`
+    },
+    body: JSON.stringify({
+      draft: false
+    })
+  }).then(r => r.json())
+}
+
+async function createPDF() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://github.com/alshdavid/portfolio/blob/master/ABRIDGED.md');
@@ -41,51 +103,8 @@ const puppeteer = require('puppeteer');
 
   await browser.close();
 
-  const URL = 'https://api.github.com/repos/alshdavid/portfolio/releases'
-  const token = process.env.GITHUB_TOKEN
-
-  const response = await fetch(URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `token ${token}`
-    },
-    body: JSON.stringify({
-      tag_name: 'latest',
-      name: 'David Alsh: Resume',
-      body: 'Latest copy of my resume, automatically compiled from the markdown files in the repository',
-      target_commitish: 'master',
-      draft: true
-    })
-  }).then(r => r.json())
-
-  const form = new FormData();
-
-  const uploadUrl = response.upload_url.replace('{?name,label}', '?name=resume.pdf')
-
-  form.append('file', fs.readFileSync('resume.pdf'), {
-    contentType: 'application/pdf',
-    name: 'file',
-    knownLength: fs.statSync("resume.pdf").size,
-    filename: 'resume.pdf',
-  });
-
-  await fetch(uploadUrl, {
-    method: 'POST',
-    headers: {
-      'Authorization': `token ${token}`,
-    },
-    body: form
-  }).then(r => r.json())
-
-  await fetch(`${URL}/${response.id}`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `token ${token}`
-    },
-    body: JSON.stringify({
-      draft: false
-    })
-  }).then(r => r.json())
-})();
+  return {
+    buffer: fs.readFileSync('resume.pdf'),
+    stats: fs.statSync('resume.pdf')
+  }
+}
